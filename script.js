2f47c8004168b1a228cc7c5068db2304b7910e2c
WelitonPro <132025795+WelitonPro@users.noreply.github.com>
2023-04-27 21:49:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    const bmi = (weight / (height * height)).toFixed(2);
+
+    const value = document.getElementById('value');
+    let description = '';
+
+    value.classList.add('attention');
+
+    document.getElementById('infos').classList.remove('hidden');
+
+    if (bmi < 18.5) {
+        description = 'Cuidado! Você está abaixo do peso.';
+        value.classList.remove('attention');
+        value.classList.add('above');
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+        description = "Você está no peso ideal.";
+        value.classList.remove('attention');
+        value.classList.add('below');
+    } else if (bmi > 25 && bmi <= 29.0) {
+        description = "Cuidado! Você está com sobrepeso.";
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    } else if (bmi > 30 && bmi <= 34.9) {
+        description = "Cuidado! Você está com obesidade moderada.";
+    } else if (bmi > 35 && bmi <= 39.0) {
+        description = "Cuidado! Você está com obesidade severa.";
+    } else {
+        description = "Cuidado! Você está com obesidade morbida.";
+    }
+
+    value.textContent = bmi.replace('.',',');
+    document.getElementById('description').textContent = description;
+
+});
